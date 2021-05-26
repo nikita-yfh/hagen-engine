371407e5a37c4f84d874e35535942244f431b841
nikita-yfh <nikita.yfh@gmail.com>
2021-05-26 12:31:27 +0300

    Added more realistic water

--- kolobator/common/level.lua
+++ kolobator/common/level.lua
@@ -12,13 +12,20 @@ function play(name,x,y)
 	end
 end
 function Level.init()
-    local shader=Shader("water.vert","rain.frag")
-	shader:add_tex("tex1"):set("noise.png");
-	shader:add_float("time"):set(game.time);
-	shader:add_vec2("resolution"):set(graphics.display.w,graphics.display.h);
-    graphics.set_texture_shader("_all",shader)
+    --local shader=Shader("common.vert","rain.frag")
+	--shader:add_tex("tex1"):set("noise.png");
+	--shader:add_float("time"):set(game.time);
+	--shader:add_vec2("resolution"):set(graphics.display.w,graphics.display.h);
+    --graphics.set_texture_shader("_all",shader)
+    
+    local shader=Shader("common.vert","water.frag")
+	shader:add_tex("tex1"):set("diplacement.png");
+    graphics.set_texture_shader("water.png",shader)
+    
 end
 function Level.update()
-    local shader=graphics.get_shader("_all")
+    --local shader=graphics.get_shader("_all")
+	--shader:add_float("time"):set(game.time);
+    local shader=graphics.get_shader("water.png")
 	shader:add_float("time"):set(game.time);
 end
---
+++ kolobator/shaders/water.frag
@@ -0,0 +1,14 @@
+varying vec4 color;
+varying vec2 texCoord;
+
+uniform sampler2D tex;
+uniform sampler2D tex1;
+uniform float time;
+
+void main() {
+    vec2 displacement=texture2D (tex1, texCoord/1000.0).xy;
+ 
+    float t= texCoord.y + displacement.y *0.005+ (sin (texCoord.x/10.0 +time/100.0) * 0.0005);
+     
+    gl_FragColor = color * texture2D (tex, vec2 (texCoord.x, t));
+}	
