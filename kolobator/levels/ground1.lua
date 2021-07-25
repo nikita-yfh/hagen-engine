de95863d1546710a80cc3b5f423f0306c8d6547a
HaBeBu <73099219+HaBeBu@users.noreply.github.com>
2021-07-25 10:55:38 +0300

    Add files via upload

--- kolobator/levels/ground1.lua
+++ kolobator/levels/ground1.lua
@@ -11,28 +11,4 @@ function level.update()
 		body("nora"):fixture("fg").layer=127
 		body("nora"):fixture("fg").texture="ground.png"
 	end
-
-	if(world.eb_all_collide(player,body("nora2"))) then
-		body("nora2"):fixture("fg2").layer=-128
-		body("nora2"):fixture("fg2").texture="ground_dark.png"
-	else
-		body("nora2"):fixture("fg2").layer=127
-		body("nora2"):fixture("fg2").texture="ground.png"
-	end
-	if(world.eb_all_collide(player,body("nora3"))) then
-		body("nora3"):fixture("fg3").layer=-128
-		body("nora3"):fixture("fg3").texture="ground_dark.png"
-	else
-		body("nora3"):fixture("fg3").layer=127
-		body("nora3"):fixture("fg3").texture="ground.png"
-
-	end
-
-	if(world.eb_all_collide(player,body("nora4"))) then
-		body("nora4"):fixture("fg4").layer=-128
-		body("nora4"):fixture("fg4").texture="ground_dark.png"
-	else
-		body("nora4"):fixture("fg4").layer=127
-		body("nora4"):fixture("fg4").texture="ground.png"
-	end
 end
