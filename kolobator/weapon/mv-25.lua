52f41304737faf1733d7446a14b9fec0e3eb2d3f
HaBeBu <73099219+HaBeBu@users.noreply.github.com>
2021-08-10 23:22:39 +0300

    Add files via upload

---
+++ kolobator/weapon/mv-25.lua
@@ -0,0 +1,14 @@
+function mv-25.init(w,e)
+	w.dx=0.4
+	w.dy=0.05
+	w.bullet1="762"
+	w.texture="weapon/mv-25.png"
+end
+function mv-25.fire2(w,e)
+	if(not game.interval(100)) then return 0 end
+	local bullet=world.create_bullet(e,"bullet_762",100)
+	bullet.userdata.create_time=game.time
+	bullet.userdata.entity=e.id
+	play("shot.flac",e.x,e.y)
+	return 1
+end
